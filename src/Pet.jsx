266f03a40a8effbea545564638d05e0af03d9bf1
Abdallah Daknache <58996641+abdallahdak7@users.noreply.github.com>
2023-01-07 01:01:07 +0300

    Update Pet.jsx

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,7 +2,7 @@ const Pet = (props) => {
   return (
     <div>
       <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
+      <h4>{props.animal}</h4>
       <h3>{props.breed}</h3>
     </div>
   );
